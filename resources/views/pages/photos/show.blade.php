56763b28303fd330600a3650d55727afdac8d59f
Odhuu <odohuugiin@gmail.com>
2015-05-14 09:28:44 +0900

    dahin sergeesen

--- resources/views/pages/photos/show.blade.php
+++
@@ -1,7 +0,0 @@
-@extends('app')
-
-@section('content')
-    <h1>{{ $photo->name }}</h1>
-    <div>{!! Html::image($photo->path, $photo->name, array('width' => 120)) !!}</div>
-    <div>{{ $photo->path }}</div>
-@stop
